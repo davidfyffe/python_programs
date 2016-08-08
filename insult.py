fe53a827076e29ed995268e61984cd44abfb978e
David Fyffe <davidfyffe@yahoo.co.uk>
2016-08-08 22:02:18 +0000

    init upload

---
+++ insult.py
@@ -0,0 +1,7 @@
+from random import choice
+adjectives = ["wet", "big"]
+nouns = ["turnip", "dog"]
+print ("You are a")
+adjective = choice (adjectives)
+print (adjective)
+print (choice (nouns))
---
+++ try-espeak.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# try_espeak.py
+# Show use of espeak.py
+import espeak as talker
+def main():
+	talker.say("Hello World. Hello David, Diana, Zach, Oliver, Dusty")
+
+if __name__ == '__main__':
+	main()
